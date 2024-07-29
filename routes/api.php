cf9c8bfdf2b2fcf8169679b32eef46028ef8a835
Gabriel Turlea <gabrielturlea27@gmail.com>
2024-07-29 20:43:56 +0300

    update deal

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +12,6 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/webhook/deal', function (Request $request) {
-    dd($request);
     $requestData = $request->all();
     // Log the server name
 
@@ -42,7 +42,6 @@ Route::post('/webhook/deal', function (Request $request) {
 });
 
 Route::get('/webhook/person', function (Request $request) {
-    dd($request);
     $requestData = $request->all();
     // Log the server name
 
@@ -70,4 +69,43 @@ Route::post('/webhook/person', function (Request $request) {
         'name' => 'Abigail',
         'state' => 'CA',
     ]);
+});
+
+Route::get('/deal/{id}', function (Request $request, int $id) {
+    $apiToken = env('PIPEDRIVE_API_TOKEN');
+    $baseUrl = env('PIPEDRIVE_URL');
+    // Create a new Guzzle client
+    $client = new Client();
+    $data = ['id' => $id, 'value' => rand(100,5000)];
+    // Prepare the URL with the deal ID
+    $url = "{$baseUrl}/api/v2/deals/{$id}?api_token={$apiToken}";
+    try {
+        // Make the PATCH request
+        $response = $client->request('PATCH', $url, [
+            'json' => $data,
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        // Get the response body and status code
+        $statusCode = $response->getStatusCode();
+        $body = $response->getBody()->getContents();
+
+
+        // Return the response
+        return response()->json([
+            'status' => $statusCode,
+            'data' => json_decode($body),
+        ]);
+
+    } catch (\GuzzleHttp\Exception\RequestException $e) {
+        // Log error details
+
+        // Return error response
+        return response()->json([
+            'status' => $e->getCode(),
+            'message' => $e->getMessage(),
+        ], $e->getCode());
+    }
 });
