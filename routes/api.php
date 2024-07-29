fbdbe22a1b6ce7458ecb2c83a6eb28c307a05d77
Gabriel Turlea <gabrielturlea27@gmail.com>
2024-07-29 20:03:57 +0300

    add person

--- routes/api.php
+++ routes/api.php
@@ -40,3 +40,34 @@ Route::post('/webhook/deal', function (Request $request) {
         'state' => 'CA',
     ]);
 });
+
+Route::get('/webhook/person', function (Request $request) {
+    dd($request);
+    $requestData = $request->all();
+    // Log the server name
+
+    $ipAddress = $request->ip();
+    Log::info('-- PERSON GET IP CLIENT: ' . $ipAddress);
+    // Alternatively, you can log the request data as a JSON string
+    Log::info('Request Data: ' . json_encode($requestData));
+
+    return response()->json([
+        'name' => 'Abigail',
+        'state' => 'CA',
+    ]);
+});
+
+Route::post('/webhook/person', function (Request $request) {
+    $requestData = $request->all();
+    // Log the server name
+
+    $ipAddress = $request->ip();
+    Log::info('-- PERSON POST IP CLIENT: ' . $ipAddress);
+    // Alternatively, you can log the request data as a JSON string
+    Log::info('Request Data: ' . json_encode($requestData));
+
+    return response()->json([
+        'name' => 'Abigail',
+        'state' => 'CA',
+    ]);
+});
